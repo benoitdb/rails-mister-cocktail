bf0df6d26c5370b41c1ee1344e1bd8a8ca8a8638
Benoit Dejean <benoit.dejeandelabatie@gmail.com>
2018-03-29 16:55:39 +0200

    Cocktail doses & ingredients models view controllers routes added

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+# A
+class Ingredient < ApplicationRecord
+  has_many :doses
+  has_many :cocktails, through: :doses
+  validates :name, uniqueness: true, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts 'Creating data...'
+
+5.times do
+cocktail = Cocktail.create!(
+  name: Faker::Hipster.sentence(2)
+)
+
+  4.times do
+  ingredient = Ingredient.create!(
+    name: Faker::Food.ingredient
+  )
+    1.times do
+      dose = Dose.create!(
+        description: Faker::Number.between(1, 10),
+        cocktail_id: cocktail.id,
+        ingredient_id: ingredient.id
+      )
+    end
+  end
+end
+
+puts 'Finished!'
